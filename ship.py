a25b810b221b473591f0be83e06fa17c802cf296
legeonerkd <legeonerkd@gmail.com>
2021-08-17 21:50:14 +0300

    created Ship

--- ship.py
+++ ship.py
@@ -1,3 +1,10 @@
 class Ship:
-    def __init__(self, size):
+    def __init__(self, size, coord_x, coord_y, direction):
         self.size = size
+        self.coord_x = coord_x
+        self.coord_y = coord_y
+        self.direction = direction
+
+    def get_ship(self, ship_cell):
+        self.ship_cell = ship_cell
+        return [self.ship_cell] * self.size
